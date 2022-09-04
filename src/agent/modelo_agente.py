4d745a0de330d68c3a701d90284542415f207d80
giovanadionisio <giovanadionisio.gd@gmail.com>
2022-09-04 15:47:59 -0300

    corrigindo erros de merge Co-authored-by: Murilo Gomes <murilo.goomes@hotmail.com> Co-authored-by: Giovannabbottino <giovannabbottino@gmail.com> Co-authored-by: gabrielapivetta <gpivetta99@gmail.com> Giuulob89 <giulialobo89@gmail.com> Co-authored-by: giovanadionisio <giovanadionisio.gd@gmail.com> Co-authored-by: hryds <rodriguerya@gmail.com>

--- src/agent/modelo_agente.py
+++ src/agent/modelo_agente.py
@@ -5,6 +5,13 @@ from agent.agente_coletor import AgenteColetor
 from agent.agente_construtor import AgenteConstrutor
 from model.mapa import Mapa
 from utils.utils import posicao_aleatoria
+from random import randrange
+from mesa.space import MultiGrid
+
+def genPos():
+    x = randrange(10)
+    y = randrange(10)
+    return x, y
 
 class ModeloAgente(Model):
     def __init__(self, mapa: Mapa, num_agentes_coletor: int = 2, num_agentes_construtor: int = 2) -> None:
@@ -12,14 +19,27 @@ class ModeloAgente(Model):
         self.num_agentes_construtor = num_agentes_construtor
         self.mapa = mapa
         self.schedule = mesa.time.RandomActivation(self)
+        self.grid = MultiGrid(10,10, True)
+        self.running = True
+        self.filledPositions = []
 
         for i in range(1, num_agentes_coletor + 1):
-            agente_coletor = AgenteColetor(i, self, posicao_aleatoria(), posicao_aleatoria())
-            self.schedule.add(agente_coletor)
+            x, y = genPos()
+            while (x,y) in self.filledPositions:
+                x, y = genPos()
+            self.filledPositions.append((x,y))
+            agente = AgenteColetor(i, self, posicao_aleatoria(), posicao_aleatoria())
+            self.schedule.add(agente)
+            self.grid.place_agent(agente, (x , y))
         
         for i in range(num_agentes_coletor + 1, num_agentes_coletor + num_agentes_construtor + 1):
-            agente_construtor = AgenteConstrutor(i, self, posicao_aleatoria(), posicao_aleatoria())
-            self.schedule.add(agente_construtor)
+            x, y = genPos()
+            while (x,y) in self.filledPositions:
+                x, y = genPos()
+            self.filledPositions.append((x,y))
+            agente = AgenteConstrutor(i, self, posicao_aleatoria(), posicao_aleatoria())
+            self.schedule.add(agente)
+            self.grid.place_agent(agente, (x , y))
 
     def step(self) -> None:
         return self.schedule.step()
--- src/canvas.py
+++ src/canvas.py
@@ -1,8 +1,9 @@
 from mesa.visualization.modules import CanvasGrid
 from mesa.visualization.ModularVisualization import ModularServer
 from mesa.visualization.UserParam import UserSettableParameter
-from agent.modelo_coletor import ModeloColetor
-from agent.agente_mapa import AgenteMapa
+from agent.modelo_agente import ModeloAgente
+from model.mapa import Mapa
+
 
 def agentPortrayal(agent):
     portrayal = {"Shape": "assets/coletor.png",
@@ -14,11 +15,11 @@ def agentPortrayal(agent):
 
 canvas_element = CanvasGrid(agentPortrayal, 10, 10, 500, 500)
 
-mapa = AgenteMapa(11, 11)
+mapa = Mapa(11, 11)
 mapa.preenche_mapa(10)
 
 server =  ModularServer(
-    ModeloColetor, [canvas_element], "Mapa", {"num_agentes":1, "mapa":mapa}
+    ModeloAgente, [canvas_element], "Mapa", {"mapa":mapa}
 )
 
 server.port = 8521
